96806c5d83a97dea9bd5463ae8aa3f676040bf15
MansurTsutiev <nyse_tm@yahoo.com>
2018-02-05 17:28:30 -0500

    implement func to add new item to an existing list

--- app/components/new-item-form.js
+++ app/components/new-item-form.js
@@ -1,4 +1,16 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    toggleForm() {
+      this.set('clicked', true);
+    },
+    saveItem(e) {
+      e.preventDefault();
+      this.onsubmit(this.title, this.list);
+      this.set('title', '');
+      this.set('clicked', false);
+    }
+
+  }
 });

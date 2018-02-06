b30f7596bc3fa0a7e999f070d7e0963ca07a7b97
MansurTsutiev <nyse_tm@yahoo.com>
2018-02-06 00:05:15 -0500

    set up default list

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -8,23 +8,7 @@ export default function(server) {
   // server.createList('post', 10);
 
   server.create('item', {
-    title: 'Ruby',
-    list: 'BackLog',
-    description: 'Description 1'
-  });
-  server.create('item', {
-    title: 'Rails',
-    list: 'BackLog',
-    description: 'Description 2'
-  });
-  server.create('item', {
-    title: 'Ember',
-    list: 'Next',
-    description: 'Description 3'
-  });
-  server.create('item', {
-    title: 'Bloomberg',
-    list: 'Next',
-    description: 'Description 4'
+    list: 'Default List',
+    description: ''
   });
 }
